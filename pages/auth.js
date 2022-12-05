b776de09ae82bf0709be11da891f38bc77f7367d
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-12-05 03:07:57 -0800

    removed wrong imports

--- pages/auth.js
+++ pages/auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Navbar from '../components/Navbar'
 import axios from 'axios'
 import Alert from '../components/Alert'
