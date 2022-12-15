4b3442971ae1be3686101935f52a9f94f352c93e
Cyrus <cyrusb10@gmail.com>
2022-12-14 19:58:10 -0800

    license

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,28 @@
+<<<<<<< Updated upstream
 import { useState, useEffect } from 'react'
+=======
+// Copyright 2022 Cyrus Baybay Sean Hatfield Rina Watanabe 
+
+// cyrusbaybay@csu.fullerton.edu
+
+// shatfield4@csu.fullerton.edu
+
+// rinawata@csu.fullerton.edu
+
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+>>>>>>> Stashed changes
 import '../styles/globals.css'
 
 
